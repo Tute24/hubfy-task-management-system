b0415f743fa33557d0672d8481936cae772218d0
Arthur Araujo <arthurtute200@hotmail.com>
2025-12-13 16:16:10 -0300

    feat: created the prisma tasks repository

---
+++ src/modules/tasks/repositories/prisma-tasks-repository.ts
@@ -0,0 +1,29 @@
+import { Prisma } from '@prisma/client';
+import { TasksRepository } from '../interfaces/tasks-repository-interface';
+import prisma from '@/core/lib/prisma';
+
+export class PrismaTasksRepository implements TasksRepository {
+  async createMany(data: Prisma.TaskCreateManyInput[]) {
+    const task = await prisma.task.createMany({ data }); //optei por usar o createMany aqui, pois no frontend darei a opção de criar várias tasks de uma vez
+
+    return task;
+  }
+
+  async getAll() {
+    const tasks = await prisma.task.findMany();
+
+    return tasks;
+  }
+
+  async findById(id: number) {
+    const task = await prisma.task.findUnique({ where: { id } });
+
+    return task;
+  }
+
+  async delete(id: number) {
+    const task = await prisma.task.delete({ where: { id } });
+
+    return task;
+  }
+}

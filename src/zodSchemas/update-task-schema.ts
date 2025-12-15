5b0565e95042ae5c95dab69c70ee686c687825ab
Arthur Araujo <arthurtute200@hotmail.com>
2025-12-15 20:24:56 -0300

    refactor: adjusted error messages on zodSchemas

--- src/zodSchemas/update-task-schema.ts
+++ src/zodSchemas/update-task-schema.ts
@@ -2,11 +2,15 @@ import z from 'zod';
 
 const updateTaskSchema = z.object({
   title: z
-    .string({ message: 'Enter a valid title with at 2 least 2 characters.' })
-    .min(2)
+    .string()
+    .min(2, { message: 'Enter a valid title with at 2 least 2 characters.' })
     .optional(),
   description: z.string().optional(),
-  status: z.enum(['PENDING', 'IN_PROGRESS', 'COMPLETED']).optional(),
+  status: z
+    .enum(['PENDING', 'IN_PROGRESS', 'COMPLETED'], {
+      message: `Values must only be 'PENDING', 'IN_PROGRESS' or 'COMPLETED'`,
+    })
+    .optional(),
 });
 
 export default updateTaskSchema;

c9852dcc7306041c5d9308d70c05d635a03eb673
Arthur Araujo <arthurtute200@hotmail.com>
2025-12-15 21:20:45 -0300

    refactor: refactoring general store to not persist

--- src/zustand-stores/general/general.store.ts
+++ src/zustand-stores/general/general.store.ts
@@ -1,23 +1,13 @@
 import { GeneralStoreType } from '@/types/zustand-store-types/general-store/general-store-types';
 import { create } from 'zustand';
-import { createJSONStorage, persist } from 'zustand/middleware';
 
 export const useGeneralStore = create<GeneralStoreType>()(
-  persist(
-    (set): GeneralStoreType => ({
-      isLoading: false,
-      statusMessage: null,
-      hasHydrated: false,
+  (set): GeneralStoreType => ({
+    isLoading: false,
+    statusMessage: null,
+    hasHydrated: false,
 
-      setIsLoading: (isLoading) => set({ isLoading }),
-      setStatusMessage: (statusMessage) => set({ statusMessage }),
-    }),
-    {
-      name: 'general-store',
-      storage: createJSONStorage(() => sessionStorage),
-      onRehydrateStorage: () => (state) => {
-        if (state) state.hasHydrated = true;
-      },
-    },
-  ),
+    setIsLoading: (isLoading) => set({ isLoading }),
+    setStatusMessage: (statusMessage) => set({ statusMessage }),
+  }),
 );
